97411f5f9b96c2cfd4e307936898a2bc716734b1
James Taylor <jostylr@gmail.com>
2016-12-25 12:28:15 -0500

    v 0.14.1 updated lib to v1.11.1 to address bad reporting in cd

--- index.js
+++ index.js
@@ -696,7 +696,7 @@ var opts = require("nomnom").
             flag : true,
             help : "version number",
             callback : function () {
-                return "v.0.14.0";
+                return "v.0.14.1";
             }
         },
         scopes: {
